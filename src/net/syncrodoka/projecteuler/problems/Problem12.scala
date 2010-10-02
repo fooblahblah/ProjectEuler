a1f16d2bceefff15396fee85bee20b7bd4d78a27
Jeff Simpson <jeff@syncrodoka.net>
2010-10-02 17:28:06 -0600

    blah

--- src/net/syncrodoka/projecteuler/problems/Problem12.scala
+++ src/net/syncrodoka/projecteuler/problems/Problem12.scala
@@ -3,7 +3,6 @@ package net.syncrodoka.projecteuler.problems
 import net.syncrodoka.projecteuler.Numerics
 
 object Problem12 {
-
 	def main(args: Array[String]) = {			
 		val i = Numerics.from(BigInt(1)).map(n => BigInt(1) to n).map(m => m.sum).
 			dropWhile(o => Numerics.factors(o).length * 2 < 500).iterator
